8baf3aaf0710fe5e90c127cecee15041be33a82c
jeetrajput001 <vishwjeetsingh83@gmail.com>
2025-06-09 16:14:33 +0530

    first commit

---
+++ src/test/java/com/springboot/hogwarts_artifacts_online/HogwartsArtifactsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.hogwarts_artifacts_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactsOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
